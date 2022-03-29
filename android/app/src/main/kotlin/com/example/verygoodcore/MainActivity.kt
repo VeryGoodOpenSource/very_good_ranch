0a8c31be0d2890d9aacae80222fe6132fd599c3e
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2022-03-29 18:09:35 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.very_good_ranch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:very_good_ranch/app/app.dart';
+import 'package:very_good_ranch/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}

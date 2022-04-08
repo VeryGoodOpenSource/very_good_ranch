6c98cd45fa6af31b6038c5176e3f8c0450838c28
Erick <erickzanardoo@gmail.com>
2022-04-08 09:29:02 -0300

    feat: adding some art assets to the title page (#19)
    * feat: adding some art assets to the title page
    
    * fix: coverage
    
    * fix: title screen layout
    
    * fix: wrong merge
    
    Co-authored-by: Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>

--- lib/title/view/title_page.dart
+++ lib/title/view/title_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:very_good_ranch/credits/credits.dart';
 import 'package:very_good_ranch/game/game.dart';
+import 'package:very_good_ranch/gen/assets.gen.dart';
 import 'package:very_good_ranch/l10n/l10n.dart';
 import 'package:very_good_ranch/settings/settings.dart';
 
@@ -10,52 +11,71 @@ class TitlePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF131433),
       body: Center(
-        child: Column(
-          children: [
-            Expanded(
-              child: Center(
-                child: Text(
-                  context.l10n.gameTitle,
-                  style: Theme.of(context).textTheme.headline2,
-                  textAlign: TextAlign.center,
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 500),
+          child: Stack(
+            children: [
+              const Positioned.fill(
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Color(0xFF4776F7), Color(0xFFFFFFFF)],
+                      begin: Alignment.center,
+                      end: Alignment.topCenter,
+                    ),
+                  ),
                 ),
               ),
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushReplacement<void, void>(
-                        GamePage.route(),
-                      );
-                    },
-                    child: Text(context.l10n.play),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      showDialog<void>(
-                        context: context,
-                        builder: (_) => const CreditsDialog(),
-                      );
-                    },
-                    child: Text(context.l10n.credits),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      showDialog<void>(
-                        context: context,
-                        builder: (_) => const SettingsDialog(),
-                      );
-                    },
-                    child: Text(context.l10n.settings),
-                  ),
-                ],
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Image.asset(
+                  Assets.images.titleHills.path,
+                ),
               ),
-            ),
-          ],
+              Positioned.fill(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      Assets.images.titleHeader.path,
+                      semanticLabel: context.l10n.gameTitle,
+                    ),
+                    const SizedBox(height: 64),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pushReplacement<void, void>(
+                          GamePage.route(),
+                        );
+                      },
+                      child: Text(context.l10n.play),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        showDialog<void>(
+                          context: context,
+                          builder: (_) => const CreditsDialog(),
+                        );
+                      },
+                      child: Text(context.l10n.credits),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        showDialog<void>(
+                          context: context,
+                          builder: (_) => const SettingsDialog(),
+                        );
+                      },
+                      child: Text(context.l10n.settings),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

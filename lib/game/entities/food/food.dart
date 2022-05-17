60698990ae9e6a8c9478171415f0335a9e5719cb
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2022-05-17 13:25:08 +0200

    feat: make food more rare and nutritious (#45)
    * feat: make food more rare and nutritious
    
    * feat: make food more rare and nutritious
    
    * feat: make food more rare and nutritious
    
    * feat: make food more rare and nutritious
    
    * feat: make food more rare and nutritious

--- lib/game/entities/food/food.dart
+++ lib/game/entities/food/food.dart
@@ -8,7 +8,6 @@ import 'package:very_good_ranch/game/entities/food/behaviors/behaviors.dart';
 class Food extends Entity {
   Food({
     required super.position,
-    required this.nutrition,
     required this.type,
   }) : super(
           size: Vector2.all(32),
@@ -27,47 +26,51 @@ class Food extends Entity {
   @visibleForTesting
   Food.test({
     this.type = FoodType.candy,
-    this.nutrition = 0,
     super.behaviors,
   }) : super(size: Vector2.all(32), children: [FoodComponent(type: type)]);
-
-  /// {@macro food_component}
-  ///
-  /// Constructs a cupcake.
-  Food.cupcake({
-    required Vector2 position,
-  }) : this(position: position, nutrition: 2.5, type: FoodType.cupcake);
-
-  /// {@macro food_component}
-  ///
-  /// Constructs a lollipop.
-  Food.lollipop({
-    required Vector2 position,
-  }) : this(position: position, nutrition: 1.5, type: FoodType.lollipop);
-
-  /// {@macro food_component}
-  ///
-  /// Constructs a pancake.
-  Food.pancake({
-    required Vector2 position,
-  }) : this(position: position, nutrition: 3, type: FoodType.pancake);
-
-  /// {@macro food_component}
-  ///
-  /// Constructs an ice cream.
-  Food.iceCream({
-    required Vector2 position,
-  }) : this(position: position, nutrition: 2, type: FoodType.iceCream);
 
   /// {@macro food_component}
   ///
   /// Constructs a candy.
   Food.candy({
     required Vector2 position,
-  }) : this(position: position, nutrition: 1, type: FoodType.candy);
+  }) : this(
+          position: position,
+          type: FoodType.candy,
+        );
+
+  /// {@macro food_component}
+  ///
+  /// Constructs a lollipop.
+  Food.lollipop({
+    required Vector2 position,
+  }) : this(
+          position: position,
+          type: FoodType.lollipop,
+        );
+
+  /// {@macro food_component}
+  ///
+  /// Constructs a pancake.
+  Food.pancake({
+    required Vector2 position,
+  }) : this(
+          position: position,
+          type: FoodType.pancake,
+        );
+
+  /// {@macro food_component}
+  ///
+  /// Constructs an ice cream.
+  Food.iceCream({
+    required Vector2 position,
+  }) : this(
+          position: position,
+          type: FoodType.iceCream,
+        );
 
   /// The amount of nutrition the food provides.
-  final double nutrition;
+  double get nutrition => type.nutrition;
 
   /// The type of food.
   final FoodType type;
--- packages/ranch_components/lib/src/components/food_component.dart
+++ packages/ranch_components/lib/src/components/food_component.dart
@@ -4,8 +4,8 @@ import 'package:flame/components.dart';
 
 /// Type of food.
 enum FoodType {
-  /// Cupcake type.
-  cupcake,
+  /// Candy type.
+  candy,
 
   /// Lollipop type.
   lollipop,
@@ -14,10 +14,44 @@ enum FoodType {
   pancake,
 
   /// Ice cream type.
-  iceCream,
+  iceCream;
+}
 
-  /// Candy type.
-  candy,
+/// NOTE: This should be a 2.17 enum constructor but guess what, coverage
+/// doesn't work for that, see: https://github.com/dart-lang/coverage/issues/386
+extension FoodTypeX on FoodType {
+  /// The nutrition value of the food.
+  double get nutrition {
+    switch (this) {
+      case FoodType.candy:
+        return 1;
+      case FoodType.lollipop:
+        return 2;
+      case FoodType.pancake:
+        return 3;
+      case FoodType.iceCream:
+        return 4;
+    }
+  }
+
+  /// The rarity of the food.
+  ///
+  /// Each food type has a rarity percentage between 0 and 100, where 0 is the
+  /// most rare and 100 is the most common.
+  ///
+  /// The rarity of a food is used to determine the chance of it being spawned.
+  int get rarity {
+    switch (this) {
+      case FoodType.candy:
+        return 40;
+      case FoodType.lollipop:
+        return 30;
+      case FoodType.pancake:
+        return 20;
+      case FoodType.iceCream:
+        return 10;
+    }
+  }
 }
 
 /// {@template food_component}
@@ -49,9 +83,6 @@ class FoodComponent extends PositionComponent {
     radius = size.x / 2;
     paint = Paint();
     switch (type) {
-      case FoodType.cupcake:
-        paint.color = const Color(0xFFE1BA84);
-        break;
       case FoodType.lollipop:
         paint.color = const Color(0xFF3B93FF);
         break;

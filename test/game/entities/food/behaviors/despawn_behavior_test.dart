f80541b5458ff45cd5e4d87a642bd56fd548bc4b
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2022-05-17 23:03:12 +0200

    feat: implement despawn behavior for food (#49)
    * feat: implement despawn behavior for food
    
    * feat: implement despawn behavior for food
    
    * Update lib/game/entities/food/behaviors/draggable_behavior.dart
    
    Co-authored-by: Renan <6718144+renancaraujo@users.noreply.github.com>
    
    Co-authored-by: Renan <6718144+renancaraujo@users.noreply.github.com>

---
+++ test/game/entities/food/behaviors/despawn_behavior_test.dart
@@ -0,0 +1,69 @@
+// ignore_for_file: cascade_invocations
+
+import 'package:flame_test/flame_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:very_good_ranch/game/entities/food/behaviors/behaviors.dart';
+import 'package:very_good_ranch/game/entities/food/food.dart';
+
+import '../../../../helpers/helpers.dart';
+
+void main() {
+  final flameTester = FlameTester(TestGame.new);
+
+  group('DespawnBehavior', () {
+    flameTester.testGameWidget(
+      'removes parent after given despawn time',
+      setUp: (game, tester) async {
+        final food = Food.test(
+          behaviors: [
+            DraggableBehavior(),
+            DespawnBehavior(despawnTime: 1),
+          ],
+        );
+        await game.ensureAdd(food);
+        await game.ready();
+      },
+      verify: (game, tester) async {
+        final food = game.firstChild<Food>()!;
+        game.update(0.5);
+
+        expect(food.shouldRemove, isFalse);
+
+        game.update(0.5);
+
+        expect(food.shouldRemove, isTrue);
+      },
+    );
+
+    flameTester.testGameWidget(
+      'should not remove when we are dragging the parent',
+      setUp: (game, tester) async {
+        final food = Food.test(
+          behaviors: [
+            DraggableBehavior(),
+            DespawnBehavior(despawnTime: 1),
+          ],
+        );
+        await game.ensureAdd(food);
+        await game.ready();
+      },
+      verify: (game, tester) async {
+        final food = game.firstChild<Food>()!;
+        game.update(0.5);
+        expect(food.shouldRemove, isFalse);
+
+        final gesture = await tester.createGesture();
+        await gesture.down(Offset.zero);
+        await gesture.moveTo(const Offset(100, 100));
+        await tester.pump();
+        game.update(0.5);
+        expect(food.shouldRemove, isFalse);
+
+        await gesture.up();
+        await tester.pump();
+        game.update(1);
+        expect(food.shouldRemove, isTrue);
+      },
+    );
+  });
+}

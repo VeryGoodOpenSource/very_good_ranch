351634ef1ba1c8e38417c601ad2c3c17b99bd2b4
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2022-05-19 21:28:20 +0200

    feat: inventory system

--- test/game/entities/unicorn/behaviours/fullness_decrease_behavior_test.dart
+++ test/game/entities/unicorn/behaviours/fullness_decrease_behavior_test.dart
@@ -6,13 +6,27 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockingjay/mockingjay.dart';
 import 'package:very_good_ranch/game/entities/unicorn/behaviors/behaviors.dart';
 import 'package:very_good_ranch/game/entities/unicorn/unicorn.dart';
-import 'package:very_good_ranch/game/very_good_ranch_game.dart';
+import 'package:very_good_ranch/game/game.dart';
+
+import '../../../../helpers/helpers.dart';
 
 class _MockEvolutionBehavior extends Mock implements EvolutionBehavior {}
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-  final flameTester = FlameTester(VeryGoodRanchGame.new);
+  late GameBloc gameBloc;
+
+  setUp(() {
+    gameBloc = MockGameBloc();
+    when(() => gameBloc.state).thenReturn(const GameState());
+  });
+
+  final flameTester = FlameTester<VeryGoodRanchGame>(
+    () => VeryGoodRanchGame(
+      gameBloc: gameBloc,
+      inventoryBloc: MockInventoryBloc(),
+    ),
+  );
 
   group('FullnessDecreaseBehavior', () {
     group('decreases fullness', () {

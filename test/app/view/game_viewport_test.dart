16fdaaffbe238e0dccf31731fe80c9e5c1eb339b
Renan <6718144+renancaraujo@users.noreply.github.com>
2022-05-31 11:04:04 +0100

    Update test/app/view/game_viewport_test.dart
    Co-authored-by: Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>

--- test/app/view/game_viewport_test.dart
+++ test/app/view/game_viewport_test.dart
@@ -7,7 +7,7 @@ void main() {
     testWidgets('viewport too wide', (tester) async {
       tester.binding.window.physicalSizeTestValue = const Size(1000, 500);
 
-      // resets the screen to its original size after the test end
+      // Resets the screen to its original size after the test ends
       addTearDown(tester.binding.window.clearPhysicalSizeTestValue);
 
       await tester.pumpWidget(

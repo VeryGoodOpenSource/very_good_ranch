dc1617a0b2f8417f36c864791a296d8516d43a8b
Felix Angelov <felangelov@gmail.com>
2022-09-15 17:02:50 -0500

    minor tweaks

--- lib/config.dart
+++ lib/config.dart
@@ -7,27 +7,27 @@ class Config {
   static const unicornVaryThresholdBy = 0.5;
 
   /// Food Spawn
-  static const foodSpawnThreshold = 15.0;
-  static const foodInitialSpawnThreshold = 12.0;
+  static const foodSpawnThreshold = 5.0;
+  static const foodInitialSpawnThreshold = 10.0;
   static const foodVaryThresholdBy = 0.2;
   static const foodSpawnDecayRate = 0.1;
 
   /// Fullness
   static const fullnessDecreaseInterval = 0.5;
   static const fullnessDecreaseFactor = StageProperty(
-    baby: 0.005,
-    child: 0.01,
-    teen: 0.015,
-    adult: 0.02,
+    baby: 0.0050,
+    child: 0.010,
+    teen: 0.0150,
+    adult: 0.020,
   );
 
   /// Enjoyment
   static const enjoymentDecreaseInterval = 0.5;
   static const enjoymentDecreaseFactor = StageProperty(
-    baby: 0.0020,
-    child: 0.0015,
-    teen: 0.0010,
-    adult: 0.0005,
+    baby: 0.0100,
+    child: 0.0050,
+    teen: 0.0030,
+    adult: 0.0015,
   );
 
   /// Feeding
